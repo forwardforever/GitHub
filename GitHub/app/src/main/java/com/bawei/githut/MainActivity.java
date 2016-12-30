b934ac78d098c9fb2164bf6a7e8a4d32f6456906
forwardforever <jnbfeng@163.com>
2016-12-30 14:19:30 +0800

    第四次提交

--- GitHub/app/src/main/java/com/bawei/githut/MainActivity.java
+++ GitHub/app/src/main/java/com/bawei/githut/MainActivity.java
@@ -2,16 +2,17 @@ package com.bawei.githut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
         TextView tv = (TextView) findViewById(R.id.tv);
-        TextView iv = (TextView) findViewById(R.id.iv);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
 
     }
 }
